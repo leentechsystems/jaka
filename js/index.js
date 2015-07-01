11d6e0a023080019b1d043253868c21dfc17e70d
Patric <patric@leentechsystems.com>
2015-07-01 14:11:42 +0800

    modified:   js/index.js

--- js/index.js
+++ js/index.js
@@ -49,7 +49,7 @@ function onOnline() {
     $("#message").addClass('hide');
     $("#main_message").addClass('hide');
     $("#sub_message").addClass('hide');
-    window.location = "192.168.2.28";
+    window.location = "http://192.168.2.28";
      // setTimeout(function(){
      //        var ref = window.open('http://www.wufoo.com/html5/attributes/07-accept.html', '_blank', 'location=no');
      //        ref.addEventListener('loaderror', function(event) { ref.close(); location.reload(); });
